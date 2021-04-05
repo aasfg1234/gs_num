c0891bfdb18ad8994e11be843af0810f87ba975b
Shawn Fan <aasfg1234@gmail.com>
2021-04-05 17:10:33 +0800

    auto guess ver use setting num

--- gs_rdSS.py
+++ gs_rdSS.py
@@ -1,18 +1,20 @@
 import random
 x = 0
-y = 1
-z = 100
-r = random.randint(1, 100)
+start = input('最小多少: ')
+end = input('最大多少: ')
+start = int(start)
+end = int(end)
+r = random.randint(start, end)
 while True:
 	x = x + 1
-	num = random.randint(y, z)
+	num = random.randint(start, end)
 	print('答案是: ', num)
 	if num == r:
 		print('猜對了，你猜了', x, '次')
 		break
 	elif num < r:
 		print('再大一點，你猜了', x, '次')
-		y = num + 1
+		start = num + 1
 	else:
 		print('小一點拉~~~，你猜了', x, '次')
-		z = num - 1
+		end = num - 1
